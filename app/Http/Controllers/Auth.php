258a95fdfff2c4375bce8bbe681e1bff41221468
safo4eg <stas.rakov111@mail.ru>
2023-11-14 05:06:27 +0300

    реализован экшен Auth->login() роута /login

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\SignupRequest;
 use App\Models\User;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as AuthFacade;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class Auth extends Controller
@@ -25,7 +26,19 @@ class Auth extends Controller
 
     public function login(LoginRequest $request)
     {
+        $payload = $request->validated();
+        $user = User::where('login', $payload['login'])->first();
+        if(!Hash::check($payload['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'login' => 'The provided credentials are incorrect'
+            ]);
+        }
 
+        return new JsonResponse([
+            'data' => [
+                'token' => $user->createToken('token')->plainTextToken
+            ]
+        ]);
     }
 
     public function logout(Request $request)
