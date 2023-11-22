0e656f0a96a528c0634a87c0447acc753034c1c5
safo4eg <stas.rakov111@mail.ru>
2023-11-22 23:10:15 +0300

    RatingController: реализован экшен update, UpdateRatingRequest: валидация данных для обновления рейтинга

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -3,21 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Rating\StoreRatingRequest;
+use App\Http\Requests\Rating\UpdateRatingRequest;
 use App\Http\Resources\RatingResource;
 use App\Models\Rating;
 use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
     public function store(StoreRatingRequest $request)
     {
         $payload = $request->validated();
         $rating = Rating::create($payload);
         return new RatingResource($rating);
     }
-    public function update(Request $request, Rating $rating)
+    public function update(UpdateRatingRequest $request, Rating $rating)
     {
-        //
+        $newRating = $request->validated()['rating'];
+        $rating->update(['rating' => $newRating]);
+        return new RatingResource($rating);
     }
     public function destroy(Rating $rating)
     {
---
+++ app/Http/Requests/Rating/UpdateRatingRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Rating;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateRatingRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [
+            'rating.in' => 'The rating must contain values from 1 to 5 inclusive'
+        ];
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'exists:users,id'],
+            'rating' => ['required', Rule::in([1, 2, 3, 4, 5])]
+        ];
+    }
+}
