b92adff0dfa5f83324fc7a31209053edcc5e356c
safo4eg <stas.rakov111@mail.ru>
2023-11-24 22:15:59 +0300

    Зарегистрированы псевдонимы для полиморфных связей

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,26 @@
 
 namespace App\Providers;
 
+use App\Models\Book;
+use App\Models\Comment;
+use App\Models\Critique;
+use App\Models\Review;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Relation::enforceMorphMap([
+            'review' => Review::class,
+            'comment' => Comment::class,
+            'critiques' => Critique::class
+        ]);
     }
 }
