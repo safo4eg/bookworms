89f12219e918d32f50071f932a82fa94b6d7c043
safo4eg <stas.rakov111@mail.ru>
2023-11-27 00:37:02 +0300

    CommentReplyStore-store():Реализовано добавление ответа на комментарий/ответ комментарий.

--- app/Http/Controllers/CommentReplyController.php
+++ app/Http/Controllers/CommentReplyController.php
@@ -2,17 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Reply\StoreReplyRequest;
+use App\Http\Resources\ReplyResource;
+use App\Models\Comment;
+use App\Models\Reply;
 use Illuminate\Http\Request;
 
 class CommentReplyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only('store');
+    }
+
     public function index()
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(StoreReplyRequest $request, Comment $comment)
     {
-        return 'test';
+        $replyId = $request->input('reply_id');
+        $payload = [
+            'text' => $request->input('text'),
+            'user_id' => $request->user()->id,
+            'comment_id' => $comment->id
+        ];
+
+        $reply = null;
+        if(isset($replyId)) {
+            $parentReply = Reply::where('id', $replyId)->first();
+            $reply = Reply::create($payload, $parentReply);
+        } else {
+            $reply = Reply::create($payload);
+        }
+
+        return new ReplyResource($reply);
     }
 }
