225e5f42ef54801ceb7c0ad6cf949981729fadf8
safo4eg <stas.rakov111@mail.ru>
2023-11-27 00:29:21 +0300

    Добавлены отношения и аксессоры в модель Reply, создан ReplyResource

---
+++ app/Http/Resources/ReplyResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\Comment\CommentResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReplyResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'text' => $this->text,
+            'comment' => ['id' => $this->comment->id],
+            'user' => new UserResource($this->user),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Kalnoy\Nestedset\NodeTrait;
 
 class Reply extends Model
@@ -14,4 +15,24 @@ class Reply extends Model
     protected $table = 'replies';
     protected $primaryKey = 'id';
     protected $guarded = [];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function comment()
+    {
+        return $this->belongsTo(Comment::class, 'comment_id', 'id');
+    }
 }
