431203a3a0e9b4c6653a27bf87138f96946d77dd
safo4eg <stas.rakov111@mail.ru>
2023-11-28 00:37:28 +0300

    Рефакторинг ресурсов

--- app/Http/Resources/Comment/CommentableResource.php
+++ app/Http/Resources/Comment/CommentableResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources\Comment;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Reply;
 
 class CommentableResource extends JsonResource
 {
@@ -13,7 +15,9 @@ class CommentableResource extends JsonResource
             'id' => $this->id,
             'text' => $this->text,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'updated_at' => $this->updated_at,
+            'user' => new UserResource($this->user),
+            'replies' => Reply\CommentResource::collection($this->replies)
         ];
     }
 }
--- app/Http/Resources/Reply/CommentResource.php
+++ app/Http/Resources/Reply/CommentResource.php
@@ -1,20 +1,25 @@
 <?php
 
-namespace App\Http\Resources\Critique;
+namespace App\Http\Resources\Reply;
 
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BookResource extends JsonResource
+class CommentResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'text' => $this->text,
+            'user' => new UserResource($this->user),
+            'children' => $this->when(
+                isset($this->children),
+                self::collection($this->children)
+            ),
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'updated_at' => $this->updated_at
         ];
     }
 }
