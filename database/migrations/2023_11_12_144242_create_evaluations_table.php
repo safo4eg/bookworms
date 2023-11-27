f9b0a786b46c8ba649186ff784e8570bb8ecef01
safo4eg <stas.rakov111@mail.ru>
2023-11-27 13:09:18 +0300

    evaluations table: рефакторинг миграций

--- database/migrations/2023_11_12_144242_create_evaluations_table.php
+++ database/migrations/2023_11_12_144242_create_evaluations_table.php
@@ -6,23 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->unsignedTinyInteger('evaluation_type_id');
             $table->morphs('evaluationable');
 
-            $table->foreign('evaluation_type_id')->references('id')->on('evaluation_types');
+            $table->foreign('evaluation_type_id')
+                ->references('id')
+                ->on('evaluation_types')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('evaluations');
