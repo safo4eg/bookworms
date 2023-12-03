690b5de17dc6048b6e8749c3dc97cb056ed938d2
safo4eg <stas.rakov111@mail.ru>
2023-12-03 14:10:13 +0300

    Реализовано добавление баллов за написание отзыва

--- app/Listeners/UserPointsAddingSubscriber.php
+++ app/Listeners/UserPointsAddingSubscriber.php
@@ -3,13 +3,19 @@
 namespace App\Listeners;
 
 use App\Events\UserPointsAdding;
+use App\Models\Review;
 use Illuminate\Support\Facades\Log;
 
 class UserPointsAddingSubscriber
 {
-    public function handlePointsAdding()
+    public function handlePointsAdding($event)
     {
-        //
+
+        if($event->trigger::class === Review::class) {
+            $user = $event->trigger->user;
+            $points = $user->points + 1;
+            $user->update(['points' => $points]);
+        }
     }
 
     public function subscribe($event)
