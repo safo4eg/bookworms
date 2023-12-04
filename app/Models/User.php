1872e7e461307c551575ee7c135fa71ade36d074
safo4eg <stas.rakov111@mail.ru>
2023-12-04 13:40:38 +0300

    model User: добавлен метод изменения ранков. UserObserver: тригер изменения ранка при смене баллов

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,26 @@ class User extends Authenticatable
         return ($this->role->title === 'Модератор')? true: false;
     }
 
+    public function changeRank()
+    {
+        $rank = Rank::class;
+        if($this->points >= 0 AND $this->points < 100) {
+            $rank::where('title', 'Новичок')->first();
+        } else if($this->points >= 100 AND $this->points < 500) {
+            $rank::where('title', 'Знаток')->first();
+        } else if($this->points >= 500 AND $this->points < 1000) {
+            $rank::where('title', 'Профи')->first();
+        } else if($this->points >= 1000 AND $this->points < 2500) {
+            $rank::where('title', 'Мудрец')->first();
+        } else if($this->points >= 2500 AND $this->points < 5000) {
+            $rank::where('title', 'Вассерман')->first();
+        }
+
+        User::withoutEvents(function () use($rank) {
+            $this->update(['rank_id' => $rank->id]);
+        });
+    }
+
     public function ratings()
     {
         return $this->hasMany(Rating::class, 'user_id', 'id');
